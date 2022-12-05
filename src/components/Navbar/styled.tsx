0829637f2550fa5dcb2c03de729aacc64a07f091
Ali Bagheri <alibagheri20ab79@gmail.com>
2022-12-05 22:55:40 +0330

    :sparkles: feat: create custom brand logo and add navbar brand component

---
+++ src/components/Navbar/styled.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+const Brand = styled('li')`
+  margin: 20px 25px 50px;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  font-size: 1.5rem;
+`;
+
+export { Brand };
