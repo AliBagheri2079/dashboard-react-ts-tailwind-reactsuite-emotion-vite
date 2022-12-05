1067f5fc7cae4a03e42fca1cd5df6dabe6fd9eb9
Ali Bagheri <alibagheri20ab79@gmail.com>
2022-12-05 23:16:48 +0330

    :sparkles: feat: add navbar items component with emotion styled

--- src/components/Navbar/styled.tsx
+++ src/components/Navbar/styled.tsx
@@ -8,4 +8,48 @@ const Brand = styled('li')`
   font-size: 1.5rem;
 `;
 
-export { Brand };
+const Item = styled('li')`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  flex: 1 1 50px;
+
+  position: relative;
+  border-radius: 2rem 0 0 2rem;
+  padding-inline-start: 10px;
+  text-transform: capitalize;
+
+  &::before,
+  &::after {
+    content: '';
+    position: absolute;
+    right: 0;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    background-color: transparent;
+  }
+
+  &::after {
+    top: -50px;
+  }
+
+  &::before {
+    bottom: -50px;
+  }
+
+  &:hover {
+    background-color: ${(props) => props.theme.color.light};
+    color: ${(props) => props.theme.color.primary};
+
+    &::before {
+      box-shadow: 35px -35px 0 10px ${(props) => props.theme.color.light};
+    }
+
+    &::after {
+      box-shadow: 35px 35px 0 10px ${(props) => props.theme.color.light};
+    }
+  }
+`;
+
+export { Brand, Item };
