be3dc6bc96a2663bb0da14608418a63ddfcdd560
Ali Bagheri <alibagheri20ab79@gmail.com>
2022-12-22 21:27:54 +0330

    :label: feat: improve performance with coins list type and enum

---
+++ src/@types/enum/index.tsx
@@ -0,0 +1,10 @@
+export enum CoinsListOrder {
+  'market_cap_desc',
+  'gecko_desc',
+  'gecko_asc',
+  'market_cap_asc',
+  'volume_asc',
+  'volume_desc',
+  'id_asc',
+  'id_desc',
+}
--- src/@types/types/index.tsx
+++ src/@types/types/index.tsx
@@ -13,3 +13,40 @@ export type ShopCardType = SliderType & {
     count: number;
   };
 };
+
+export type CoinsListType = {
+  id: string;
+  name: string;
+  symbol: string;
+  image: string;
+  current_price: number;
+
+  ath?: number;
+  high_24h?: number;
+  low_24h?: number;
+  market_cap?: number;
+  ath_change_percentage?: number;
+  price_change_percentage_24h?: number;
+  // ath_date: Date;
+  // atl: number;
+  // atl_change_percentage: number;
+  // atl_date: Date;
+  // circulating_supply: number;
+  // fully_diluted_valuation: number;
+  // last_updated: Date;
+  // market_cap_change_24h: number;
+  // market_cap_change_percentage_24h: number;
+  // market_cap_rank: number;
+  // max_supply: number;
+  // price_change_24h: number;
+  // price_change_percentage_24h_in_currency: number;
+  // roi: object;
+  // total_supply: number;
+  // total_volume: number;
+};
+
+export type CoinChartType = {
+  prices: Array<number>;
+  market_caps: Array<number>;
+  total_volumes: Array<number>;
+};
