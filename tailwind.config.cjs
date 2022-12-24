e7b70d40557d12360b9cd3854c19e8103c4c989a
Ali Bagheri <alibagheri20ab79@gmail.com>
2022-12-24 20:49:21 +0330

    :lipstick: style: add preline plugin to tailwind config file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}',
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    'node_modules/preline/dist/*.js',
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   darkMode: 'media',
-  plugins: [require('flowbite/plugin'), require('flowbite-typography')],
+  plugins: [require('flowbite/plugin'), require('preline/plugin')],
 };
