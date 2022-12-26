30bf11e8dc5e3cc111dcdc53b5a5d76f0152d176
Ali Bagheri <alibagheri20ab79@gmail.com>
2022-12-26 20:45:46 +0330

    :art: refactore: rename home page to register page

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -2,7 +2,7 @@ import Cookie from '@/components/Cookie';
 import IntroTitle from '@/components/IntroTitle';
 import SignForm from '@/components/SignForm';
 
-const MainHomeContent = () => {
+const MainRegisterContent = () => {
   return (
     <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
       <div className="grid md:grid-cols-2 items-center gap-12">
@@ -13,12 +13,12 @@ const MainHomeContent = () => {
   );
 };
 
-const Home = () => {
+const Register = () => {
   return (
     <>
-      <MainHomeContent />
+      <MainRegisterContent />
       <Cookie />
     </>
   );
 };
-export default Home;
+export default Register;
