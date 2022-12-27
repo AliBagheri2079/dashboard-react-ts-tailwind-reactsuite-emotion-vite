a62d7abea48e4f6f950d9115847f550437f03831
Ali Bagheri <alibagheri20ab79@gmail.com>
2022-12-27 04:21:36 +0330

    :art: perf: improve performance with setup route configurate

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,37 +1,81 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDarkMode } from 'usehooks-ts';
+import {
+  RouterProvider,
+  createRouteConfig,
+  createReactRouter,
+} from '@tanstack/react-router';
 
 import Layout from '@/layouts';
 import Products from '@/pages/Products';
 import Crypto from '@/pages/Crypto';
 import Register from '@/pages/Register';
 import NotFound from '@/pages/NotFound';
-import ProtectedRoute from './ProtectedRoute';
 
 /*
 ? I know routes folder created for routing in server but
 ? I think this way is possible.
 */
-const ManageRouting = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Layout />
-            </ProtectedRoute>
-          }
-        >
-          <Route index element={<Products />} />
-          <Route path="crypto" element={<Crypto />} />
-        </Route>
 
-        <Route path="/register" element={<Register />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  );
+const rootRoute = createRouteConfig();
+
+/*
+ * layout root routes
+ */
+const layoutRootRoute = rootRoute.createRoute({
+  id: 'layout',
+  component: Layout,
+});
+const indexRoute = layoutRootRoute.createRoute({
+  path: '/',
+  component: Products,
+});
+const cryptoRoute = layoutRootRoute.createRoute({
+  path: '/crypto',
+  component: Crypto,
+});
+
+/*
+ * other routes
+ */
+const registerRoute = rootRoute.createRoute({
+  path: '/register',
+  component: Register,
+});
+const notFoundRoute = rootRoute.createRoute({
+  path: '*',
+  component: NotFound,
+});
+
+/*
+ * configuration all routes
+ */
+const routeConfig = rootRoute.addChildren([
+  layoutRootRoute.addChildren([indexRoute, cryptoRoute]),
+  registerRoute,
+  notFoundRoute,
+]);
+const router = createReactRouter({ routeConfig });
+
+const ManageRouting = () => {
+  const { isDarkMode } = useDarkMode();
+  const isDarkMediaMode =
+    window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+  useEffect(() => {
+    if (isDarkMediaMode && isDarkMode) {
+      document.body.setAttribute('theme', 'dark');
+    } else {
+      document.body.setAttribute('theme', 'light');
+    }
+  }, [isDarkMediaMode, isDarkMode]);
+
+  useEffect(() => {
+    localStorage.setItem('logged', 'false');
+  }, []);
+
+  return <RouterProvider router={router} />;
 };
 
 export default ManageRouting;
